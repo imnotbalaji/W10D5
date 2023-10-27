1fc90f28e621f0260cc662b0f8e0e32bdfe2d37e
Vincent <vshan0816@gmail.com>
2023-10-27 16:33:15 -0400

    finish phase 3

--- practice-for-week-14-react-router-art-museum-long-practice-main/art-museum/src/App.js
+++ practice-for-week-14-react-router-art-museum-long-practice-main/art-museum/src/App.js
@@ -1,18 +1,27 @@
 import harvardArt from "./data/harvardArt";
 import GalleryNavigation from "./components/GalleryNavigation";
 import GalleryView from "./components/GalleryNavigation/GalleryView";
-import {Route} from 'react-router-dom';
-
+import {Route, Switch, Redirect} from 'react-router-dom';
+import Home from "./components/Home";
+import Errors from "./components/Errors";
 function App() {
   
   return (
     <div>
       <GalleryNavigation galleries={harvardArt.records} />
-      
-      <Route path="/galleries/:galleryId" >
-        <GalleryView galleries = {harvardArt.records}/>
-      </Route>
-
+      <Switch>
+        <Route path="/galleries/:galleryId" >
+          <GalleryView galleries = {harvardArt.records}/>
+        </Route>
+        <Route path="/errors">
+          <Errors />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Redirect to="/errors" />
+        
+      </Switch>
       {/* <Route path = "" component = {}/> */}
     </div>
     
---
+++ practice-for-week-14-react-router-art-museum-long-practice-main/art-museum/src/components/Errors.js
@@ -0,0 +1,7 @@
+const Errors = () => {
+    return (
+        <h2>Page Not Found</h2>
+    )
+}
+
+export default Errors
